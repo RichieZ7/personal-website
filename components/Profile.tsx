cdc9dc70c90eba025f06c5a6d38fc661ea6997f2
Richard Zhang <richard7.zhang@gmail.com>
2026-02-03 01:43:02 -0500

    google scholar icon

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -113,6 +113,24 @@ export default function Profile() {
                   <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z" />
                 </svg>
               </a>
+
+              {/* Google Scholar Icon */}
+              <a
+                href="https://scholar.google.com/citations?user=BCgZ0VEAAAAJ&hl=en"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="group relative w-12 h-12 md:w-14 md:h-14 flex items-center justify-center rounded-full bg-white hover:bg-gray-100 transition-all duration-300 hover:scale-110 hover:shadow-lg"
+                aria-label="Google Scholar"
+              >
+                <svg
+                  className="w-6 h-6 md:w-7 md:h-7 text-black group-hover:text-gray-800 transition-colors"
+                  fill="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M12 0L0 9.5l5.242 4.269C6.452 11.249 9.022 9.5 12 9.5s5.548 1.748 6.758 4.269L24 9.5 12 0zm0 10a7 7 0 1 0 0 14 7 7 0 0 0 0-14z"/>
+                </svg>
+              </a>
             </div>
           )}
         </div>
